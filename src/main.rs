f13ce0f54e0ef70e77c279bf69ac4e1aa1ae6abd
Matthew James Kraai <kraai@ftbfs.org>
2022-04-05 12:22:03 -0700

    Add a book

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,84 @@
 // License along with books.  If not, see
 // <https://www.gnu.org/licenses/>.
 
-fn main() {}
+use clap::Parser;
+use directories::ProjectDirs;
+use rusqlite::Connection;
+use std::{fs::DirBuilder, os::unix::fs::DirBuilderExt, process};
+
+#[derive(Parser)]
+struct Options {
+    title: String,
+    #[clap(name = "author", required = true)]
+    authors: Vec<String>,
+}
+
+fn main() {
+    let options = Options::parse();
+    let project_dirs = ProjectDirs::from("org.ftbfs", "", "books").unwrap_or_else(|| {
+        eprintln!("books: unable to determine home directory");
+        process::exit(1);
+    });
+    let data_dir = project_dirs.data_dir();
+    DirBuilder::new()
+        .mode(0o700)
+        .recursive(true)
+        .create(data_dir)
+        .unwrap_or_else(|e| {
+            eprintln!("books: unable to create {}: {}", data_dir.display(), e);
+            process::exit(1);
+        });
+    let database = data_dir.join("database.sqlite3");
+    let mut connection = Connection::open(&database).unwrap_or_else(|e| {
+        eprintln!("books: unable to open {}: {}", database.display(), e);
+        process::exit(1);
+    });
+    connection
+        .pragma_update(None, "FOREIGN_KEYS", 1)
+        .unwrap_or_else(|e| {
+            eprintln!("books: unable to enable foreign key constraints: {}", e);
+            process::exit(1);
+        });
+    let transaction = connection.transaction().unwrap_or_else(|e| {
+        eprintln!("books: unable to create transaction: {}", e);
+        process::exit(1);
+    });
+    {
+        transaction
+            .execute_batch(
+                "CREATE TABLE IF NOT EXISTS book (title TEXT PRIMARY KEY, completed TEXT) STRICT; CREATE TABLE IF NOT EXISTS author (title TEXT NOT NULL REFERENCES book (title) ON DELETE CASCADE ON UPDATE CASCADE, author TEXT NOT NULL, PRIMARY KEY (title, author)) STRICT;",
+            )
+            .unwrap_or_else(|e| {
+                eprintln!("books: unable to prepare statement: {}", e);
+                process::exit(1);
+            });
+        let mut statement = transaction
+            .prepare("INSERT INTO book (title) VALUES (?)")
+            .unwrap_or_else(|e| {
+                eprintln!("books: unable to prepare statement: {}", e);
+                process::exit(1);
+            });
+        statement.execute([&options.title]).unwrap_or_else(|e| {
+            eprintln!("books: unable to execute statement: {}", e);
+            process::exit(1);
+        });
+    }
+    for author in options.authors {
+        let mut statement = transaction
+            .prepare("INSERT INTO author VALUES (?, ?)")
+            .unwrap_or_else(|e| {
+                eprintln!("books: unable to prepare statement: {}", e);
+                process::exit(1);
+            });
+        statement
+            .execute([&options.title, &author])
+            .unwrap_or_else(|e| {
+                eprintln!("books: unable to execute statement: {}", e);
+                process::exit(1);
+            });
+    }
+    transaction.commit().unwrap_or_else(|e| {
+        eprintln!("books: unable to commit transaction: {}", e);
+        process::exit(1);
+    });
+}
